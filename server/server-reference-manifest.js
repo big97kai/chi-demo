70557ad89a0268f8875554e94edf9a86966e12d4
kai <yangyikai97@gmail.com>
2024-06-11 11:13:17 +0800

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"leEoUBHJhTHU7DJnPBEZa140++/WRuXB7l5CXvnSE/k=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"3sqSvjHnsDNBE8jDxoy+ksrRv5VUQkhWFhikPpKkIvo=\"}"
